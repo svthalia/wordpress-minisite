2231801699be08797247db34df27b536cd554391
Thom Wiggers <thom@thomwiggers.nl>
2018-08-15 20:09:54 +0200

    Fix permissions of theme

--- resources/copy_theme.sh
+++ resources/copy_theme.sh
@@ -2,3 +2,4 @@
 
 mkdir -p /var/www/html/wp-content/themes/
 cp -r /usr/src/minisite_theme /var/www/html/wp-content/themes/
+chown -R www-data:www-data /var/www/html/wp-content/themes/minisite_theme
